36e6d0e0bb2d0e5f1010101e4f27a9d7883f14c2
Oscar Lundberg <info@oscarlundberg.com>
2019-09-14 20:22:07 +0200

    terrainfix

--- src/terrain/terrain.js
+++ src/terrain/terrain.js
@@ -1,17 +1,14 @@
-let maxVal = 0;
 class Tile 
 {
     constructor(val) 
     {
-        if(val > maxVal) {
-            maxVal = val;
-        }
         this.types = [
             {"type":"water", "height":0.36, "color":color(127,127,255)},
             {"type":"sand", "height":0.4, "color":color(218,165,32)},
             {"type":"grass", "height":0.6, "color":color(63, 125, 32)},
-            {"type":"ground", "height":0.70, "color":color(117, 67, 32)},
-            {"type":"mountain", "height":1, "color":color(84, 87, 82)},
+            {"type":"ground", "height":0.65, "color":color(47, 74, 44)},
+            {"type":"mountain", "height":0.70, "color":color(84, 87, 82)},
+            {"type":"snow", "height":1, "color":color(220, 220, 220)},
         ];
     
         let i = 0;
@@ -30,42 +27,45 @@ class Tile
 
     draw(x, y, w, h, west, north, east, south)
     {
-        console.log("full lerp")
         noStroke();
-        let xCol = lerpColor(west.color, east.color, 0.5);
-        let yCol = lerpColor(south.color, north.color, 0.5);
-        let avgCol = lerpColor(xCol, yCol, 0.5);
-        fill(lerpColor(this.color, avgCol, 0.36));
+        if(this.newColor){
+            fill(this.newColor);
+        }
+        else if(west, north, east, south) 
+        {
+            let xCol = lerpColor(west.color, east.color, 0.5);
+            let yCol = lerpColor(south.color, north.color, 0.5);
+            let avgCol = lerpColor(xCol, yCol, 0.5);
+            this.newColor = lerpColor(this.color, avgCol, 0.5);
+            fill(this.newColor);
+
+        }
+        else if(west, north) 
+        {
+            let avgCol = lerpColor(west.color, north.color, 0.5);
+            this.newColor = lerpColor(this.color, avgCol, 0.5);
+            fill(this.newColor);
+        }
+        else {
+            fill(this.color);
+        }
         rect(x, y, w, h);
     }
 
-    draw(x, y, w, h, a, b)
-    {
-        console.log("half lerp")
-        noStroke();
-        let avgCol = lerpColor(a.color, b.color, 0.66);
-        fill(lerpColor(this.color, avgCol, 0.66));
-        rect(x, y, w, h);
-    }
-    draw(x, y, w, h)
-    {
-        noStroke();
-        fill(this.color);
-        rect(x, y, w, h);
-    }
 
     toString() {
         return this.type;
     }
 }
-let resolution = Math.PI * 4;
-
+let resolution = Math.PI * 3;
+let seed = 1;
 class TerrainGenerator 
 {
     constructor(width, height)
     {
         this.w = width;
         this.h = height;
+        noiseSeed(seed);
     }
 
     generate()
@@ -94,16 +94,33 @@ class TerrainGenerator
 
 let terrain = [];
 let size = 8;
+let sizeS;
+let resoS;
+let seedS;
 function setup() 
 {
+    seedS = createSlider(0, 255, 100);
+    resoS = createSlider(0, 25, 4);
+    sizeS = createSlider(0, 25, 8);
+    
     colorMode(RGB);
-    createCanvas(600, 600);
     let tg = new TerrainGenerator(150, 150);
     terrain = tg.generate();
+    let genB = createButton("Regenerate");
+    genB.mousePressed(()=>{
+        tg = new TerrainGenerator(150, 150);
+        terrain = tg.generate();
+        console.log("abc")
+    });
+    createCanvas(600, 600);
+
 }
 
 function draw() 
 {
+    seed = seedS.value();
+    resolution = Math.PI * resoS.value();
+    size = sizeS.value();
     frameRate(30);
     clear();
     for(let x = 0; x<terrain.length; x++) 
@@ -114,7 +131,6 @@ function draw()
             let notFirst = (x > 0 && y > 0);
             if(notFirst & notLast) 
             {
-                console.log("full lerp")
                 terrain[x][y].draw(camX + x * size, camY + y * size, size, size, terrain[x-1][y], terrain[x][y-1], terrain[x+1][y], terrain[x][y+1]);
             }
             else if(!notFirst && notLast) 
