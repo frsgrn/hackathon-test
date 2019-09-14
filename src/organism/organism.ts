dd8ab31c7b80e3e1426a86329506158c35ef8a9a
Victor Forsgren <forsgren.victor@gmail.com>
2019-09-14 12:57:51 +0200

    Organism can move

--- src/organism/organism.ts
+++ src/organism/organism.ts
@@ -1,6 +1,15 @@
 class Organism {
     genes: Number[];
+    position: {
+        x: 0,
+        y: 0
+    }
     constructor() {
         this.genes = [1,1,1,1]
+    }
+    // DIR: {x: dir_x, y: dir_y}
+    move(dir) {
+        this.position.x += dir.x;
+        this.position.y += dir.y;
     }
 }
