0e05780a7d5f23bde502a6fae6335d79b52b1199
Oscar Lundberg <info@oscarlundberg.com>
2019-09-15 13:20:25 +0200

    Update terrain.js

--- src/terrain/terrain.js
+++ src/terrain/terrain.js
@@ -1,8 +1,8 @@
 let colorSmoothing;
 
-class Tile 
+class Tile
 {
-    constructor(val) 
+    constructor(val)
     {
         this.types = [
             {"type":"water", "height":0.36, "color":color(127,127,255)},
@@ -14,18 +14,18 @@ class Tile
         ];
         this.newColor;
         let i = 0;
-        for(let t of this.types) 
+        for(let t of this.types)
         {
             if(val <= t.height)
             {
                 this.type = t.type;
-                this.color = t.color;                
+                this.color = t.color;
                 return this;
             }
         }
         this.type = this.types[0].type;
-        this.color = this.types[0].color;   
-        
+        this.color = this.types[0].color;
+
     }
 
     draw(x, y, w, h, west, north, east, south)
@@ -35,7 +35,7 @@ class Tile
         {
             fill(this.newColor);
         }
-        else if(west, north, east, south) 
+        else if(west, north, east, south)
         {
             let xCol = lerpColor(west.color, east.color, colorSmoothing);
             let yCol = lerpColor(south.color, north.color, colorSmoothing);
@@ -43,7 +43,7 @@ class Tile
             this.newColor = (lerpColor(this.color, avgCol, colorSmoothing));
             fill(this.newColor);
         }
-        else if(west, north) 
+        else if(west, north)
         {
             let avgCol = lerpColor(west.color, north.color, colorSmoothing);
             this.newColor = (lerpColor(this.color, avgCol, colorSmoothing));
@@ -64,48 +64,85 @@ let seed;
 let falloff;
 let octaves;
 
-class TerrainGenerator 
+class TerrainGenerator
 {
-    constructor(width, height)
+    constructor(width, height, xcoord, ycoord)
     {
         this.w = width;
         this.h = height;
         noiseSeed(seed);
         noiseDetail(octaves, falloff);
+        if(xcoord, ycoord){
+          this.xoff = -(xcoord * this.w);
+          this.yoff = -(ycoord * this.h);
+        }else {
+          this.xoff = 0;
+          this.yoff = 0;
+        }
+    }
+
+    draw(xoff, yoff)
+    {
+      for(let x = 0 + xoff; x < this.w + xoff; x++)
+      {
+          for(let y = 0 + yoff; y<this.h + yoff; y++)
+          {
+              let notLast = (x < this.tiles.length - 1 && y < this.tiles[0].length - 1);
+              let notFirst = (x > 0 && y > 0);
+
+              if(notFirst & notLast)
+              {
+                  this.tiles[x][y].draw(x * size,y * size, size, size, this.tiles[x-1][y], this.tiles[x][y-1], this.tiles[x+1][y], this.tiles[x][y+1]);
+              }
+              else if(!notFirst && notLast)
+              {
+                  this.tiles[x][y].draw(x * size,y * size, size, size, this.tiles[x+1][y], this.tiles[x][y+1]);
+              }
+              else if(!notLast && notFirst)
+              {
+                  this.tiles[x][y].draw(x * size,y * size, size, size, this.tiles[x-1][y], this.tiles[x][y-1]);
+              }else {
+                  this.tiles[x][y].draw(x * size,y * size, size, size,);
+              }
+          }
+      }
     }
 
     generate()
     {
         let arr = [];
-        
-        for(let x = 0; x < this.w; x++) 
+
+        for(let x = 0; x < this.w; x++)
         {
             arr[x] = []
-            for(let y = 0; y < this.h; y++) 
+            for(let y = 0; y < this.h; y++)
             {
                 arr[x][y] = 0;
             }
         }
-        
-        for(let x = 0; x < this.w; x++) 
+
+        for(let x = 0; x < this.w; x++)
         {
-            for(let y = 0; y < this.h; y++) 
+            for(let y = 0; y < this.h; y++)
             {
-                arr[x][y] = new Tile(noise(x/resolution, y/resolution));
+                arr[x][y] = new Tile(noise((x+this.xoff)/resolution, (y+this.yoff)/resolution));
             }
         }
-        return arr;
-    }    
-    
+        this.tiles = arr;
+    }
+
 }
 
-let terrain = [];
+let chunks = [];
+
+
+let chunkPos;
 
 let chunkSize;
 let size;
 
-let camX;
-let camY;
+let camX = 0;
+let camY = 0;
 
 // SLIDERS
 let chunkSizeS;
@@ -118,35 +155,48 @@ let heightS;
 let octavesS;
 let falloffS;
 
+let worldSize = 3;
 
-function setup() 
+function setup()
 {
     let container = createDiv();
     let leftCol = createDiv();
     let rightCol = createDiv();
-    
+    chunkPos = createVector(floor(worldSize/2), floor(worldSize/2));
+
     seedS = createSlider(0, 255, 101);
     resoS = createSlider(0, 25, 4);
-    sizeS = createSlider(0, 25, 8);
+    // sizeS = createSlider(0, 25, 8);
     smoothS = createSlider(0.0, 50.0, 25);
     octavesS = createSlider(0.0, 25.0, 12);
     falloffS = createSlider(0.0, 50.0, 35);
     chunkSizeS = createInput(100, "number");
-    
+
     input();
 
     camX = -(chunkSize * size)/2;
     camY = -(chunkSize * size)/2;
 
-    let tg = new TerrainGenerator(chunkSize, chunkSize);
-    terrain = tg.generate();
     let genB = createButton("Regenerate");
-    genB.mousePressed(()=>{
-        tg = new TerrainGenerator(chunkSize, chunkSize);
-        terrain = tg.generate();
-        console.log("abc")
-    });
+
+    let regen = function()
+    {
+      for(let x = 0; x<worldSize; x++)
+      {
+        chunks[x] = [];
+        for(let y = 0; y<worldSize; y++)
+        {
+          let chunk = new TerrainGenerator(chunkSize, chunkSize, x, y);
+          chunks[x][y] = chunk;
+        }
+      }
+    };
+
+    genB.mousePressed(()=>{ regen(); });
+    regen();
+
     let canv = createCanvas(600, 600);
+    size = width/chunkSize;
 
     leftCol.child(canv);
 
@@ -165,41 +215,23 @@ function setup()
     rightCol.child(createP("<br>"));
     rightCol.child(genB);
     rightCol.child(createP("<br>"));
-    rightCol.child(createP("Zoom:"));
-    rightCol.child(sizeS);
+    // rightCol.child(createP("Zoom:"));
+    // rightCol.child(sizeS);
     rightCol.addClass("list");
     container.child(leftCol);
     container.child(rightCol);
 
 }
 
-function draw() 
+function draw()
 {
     input();
     frameRate(30);
     clear();
-    for(let x = 0; x<terrain.length; x++) 
-    {
-        for(let y = 0; y<terrain[0].length; y++) 
-        {
-            let notLast = (x < terrain.length - 1 && y < terrain[0].length - 1);
-            let notFirst = (x > 0 && y > 0);
-            if(notFirst & notLast) 
-            {
-                terrain[x][y].draw(camX + x * size, camY + y * size, size, size, terrain[x-1][y], terrain[x][y-1], terrain[x+1][y], terrain[x][y+1]);
-            }
-            else if(!notFirst && notLast) 
-            {
-                terrain[x][y].draw(camX + x * size, camY + y * size, size, size, terrain[x+1][y], terrain[x][y+1]);
-            }
-            else if(!notLast && notFirst)
-            {
-                terrain[x][y].draw(camX + x * size, camY + y * size, size, size, terrain[x-1][y], terrain[x][y-1]);
-            }else {
-                terrain[x][y].draw(camX + x * size, camY + y * size, size, size);
-            }
-        }
-    }
+
+    chunks[chunkPos.x][chunkPos.y].draw(camX, camY);
+
+
     keyCheck();
 }
 
@@ -208,16 +240,16 @@ function draw()
 function input() {
     seed = seedS.value();
     resolution = Math.PI * resoS.value();
-    size = sizeS.value();
+    //size = sizeS.value();
     colorSmoothing = smoothS.value() / 100;
     octaves = octavesS.value();
     falloff = falloffS.value() / 100;
     chunkSize = chunkSizeS.value();
-    
+
 }
 
 function keyCheck() {
-    
+
 
     if(keyIsDown(65)){
         camX += size;
